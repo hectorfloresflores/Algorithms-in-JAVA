6d1553e41e33bc090516c4eab2e2c1ba924c9d00
hectorfloresflores <flores.hector.carlos@gmail.com>
2019-07-19 11:17:24 -0500

    v1

---
+++ FindtheTownJudge.java
@@ -0,0 +1,12 @@
+package com.hectorflores;
+
+public class FindtheTownJudge {
+
+	//public static boolean findthe
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
