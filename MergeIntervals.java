88b28376b4ab065621ba1b7c2187d7c3817b640d
hectorfloresflores <flores.hector.carlos@gmail.com>
2019-08-09 13:09:34 -0500

    v1

--- MergeIntervals.java
+++ MergeIntervals.java
@@ -1,8 +1,6 @@
 package com.hectorflores;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
+
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.TreeMap;
