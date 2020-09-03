9f23bc7dadd3ea8c7b03dc4b725080d3ec411125
hectorfloresflores <48966777+hectorfloresflores@users.noreply.github.com>
2020-09-03 14:41:24 -0500

    v3

--- src/main/java/com/leetcode/FirstBadVersion.java
+++ src/main/java/com/leetcode/FirstBadVersion.java
@@ -4,7 +4,10 @@ package com.leetcode;
       boolean isBadVersion(int version); */
 
 class VersionControl{
-    public boolean isBadVersion(int n) {
+    public static boolean isBadVersion(int n) {
+
+        if (n >= 1702766719) return true;
+            return false;
 
     }
 }
@@ -14,23 +17,28 @@ public class FirstBadVersion extends VersionControl {
         int left = 0;
         int right = n;
         int pos = 0;
-        Math.sum(1,2);
+        boolean isLeft = true;
         do{
-            pos = pos + ((right - left) / 2);
+            if (isLeft)
+                pos = pos + ((right - left) / 2);
+            else
+                pos = pos - ((right - left) / 2);
 
-            if (isBadVersion(pos) == false && isBadVersion(pos + 1) == true) {
+            if (!isBadVersion(pos) && isBadVersion(pos+1)) {
                 return pos + 1;
-            } else if (isBadVersion(pos) == false && isBadVersion(pos + 1) == false) {
+            } else if (!isBadVersion(pos) && !isBadVersion(pos+1)) {
                 left = pos;
+                isLeft = true;
             } else {
-                right = -pos;
+                right = pos;
+                isLeft = false;
             }
-        }while(right != 0 || left != (n - 1));
+        }while(Math.abs(left - right) > 1);
 
-        return isBad(pos);
+        return pos;
     }
 
     public static void main(String[] args) {
-        FirstBadVersion.FirstBadVersion(5);
+        FirstBadVersion.firstBadVersion(2126753390);
     }
 }
