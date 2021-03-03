5517fc859e9727f0feb70c3f7c02a3f83f840b27
hectorfloresflores <48966777+hectorfloresflores@users.noreply.github.com>
2021-03-03 11:00:25 -0600

    Leetcode Missing number

--- src/main/java/com/certification/Main.java
+++ src/main/java/com/certification/Main.java
@@ -1,11 +1,17 @@
 package com.certification;
 
 
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 class Hola{
      static String name1 = "hell";
@@ -16,14 +22,14 @@ public  abstract class Main extends Hola{
         String ll;
 
       public static void main(String[] args) throws Exception {
-          Set<Integer> set = new HashSet<>();
-          set.toArray();
-        List<Integer> list = new ArrayList<>();
-        list.add(2);
-        list.add(2);
-          System.out.println(list.subList(1,2));
-        Main m = null;
-          System.out.println(Math.round(-0.5));
+
+
+          String backupId = new SimpleDateFormat("yyyyMMdd-HHmmss").format(new Date());
+
+          Boolean siple = new SimpleDateFormat("yyyyMMdd-HHmmss")
+                  .parse(backupId).before(Date.from(Instant.now().minus(3, ChronoUnit.SECONDS)));
+
+          System.out.println(siple);
 
 
       }
